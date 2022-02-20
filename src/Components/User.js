e01354e804b5dc089679f2fbd0168256514b899e
Poojani93 <upendrapoojani@gmail.com>
2022-02-20 17:20:12 +0530

    commit1

---
+++ src/Components/User.js
@@ -0,0 +1,85 @@
+import React, {useState} from 'react'
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
+
+export default function User() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  // const classes = useStyles();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const user = {email, password}
+    console.log(user)
+    fetch("http://localhost:8080/user/add", {
+      method:"POST",
+      headers:{"Content-Type" : "application/json"},
+      body:JSON.stringify(user)
+    })
+  };
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Register Here
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value = {email}
+              onChange = {(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              autoFocus
+              value = {password}
+              onChange = {(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Submit
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
